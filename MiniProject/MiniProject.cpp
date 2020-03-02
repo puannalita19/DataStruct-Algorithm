9a96478f328d767ef705d40373c4f4f9e0fdfe4e
Puan Alma Nalita <puannalita19@gmail.com>
2020-03-03 02:03:37 +0800

    Update MiniProject.cpp

--- MiniProject/MiniProject.cpp
+++ MiniProject/MiniProject.cpp
@@ -1,7 +1,10 @@
 //Mini Project: Pizza Sales And Order Management System
-//Group member:
-//Beatrice Lim Pei Ying (A17CS0025)
-//Puan Alma Nalita (A17CS0267)
+//This is a pizza sales and order management system by using queue. This system can : 
+//-create class pizza that containts attributes of pizza 
+//-costumer can view, search, choose pizza to buy. 
+//-staff can manage order and sals and change price or pizza price. 
+//-system can generate report for the sales. 
+//-costumer can buy in set or by individual item
 
 #include <iostream>
 #include <cstring>
